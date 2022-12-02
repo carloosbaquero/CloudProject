0aa6c99538789cb7cfc8cbfacf1663b69368ba33
Joaquin Martin <jmartinacu2002@gmail.com>
2022-12-02 14:56:05 +0100

    Usando configuracion standard para linter

--- Backend/M_Content/server.js
+++ Backend/M_Content/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(upload())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.listen(PORT, () => {
