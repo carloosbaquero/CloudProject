64dd33480adbdbef21ee024ed117ba471eab664c
carloosbaquero <carlos.baqueville@gmail.com>
2022-12-14 11:13:38 +0100

    Test Fetch Api Posts

--- Frontend/src/components/Post/index.jsx
+++ Frontend/src/components/Post/index.jsx
@@ -11,6 +11,8 @@ constructor(props){
 
 render() {
 
+    const id = this.props.id;
+
     const nickname = this.props.nickname;
 
     const profilepicture= this.props.profilepicture;
@@ -21,19 +23,20 @@ render() {
 
     return <article className="Post">
 
-        <header>
+        <header className="head">
 
             <div className="Post-user">
 
             <div className="Post-user-profilepicture">
 
-                <img src="https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg" alt="John D. Veloper" />
+                {/* <img src="https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg" alt="John D. Veloper" /> */}
+                <img src={image} alt={nickname}/>
 
             </div>
 
             <div className="Post-user-nickname">
 
-                <span>John Doe</span>
+                <span>{nickname}</span>
 
             </div>
 
@@ -45,7 +48,7 @@ render() {
 
             <div className="Post-image-bg">
 
-            <img alt="Icon Living" src="https://cdn-images-1.medium.com/max/1200/1*dMSWcBZCuzyRDeMr4uE_og.png" />
+            <img alt="Icon Living" src={image} />
 
             </div>
 
@@ -53,7 +56,7 @@ render() {
 
         <div className="Post-caption">
 
-            <strong>John D. Veloper </strong> Loving Educative!
+            <strong>{nickname} </strong> Loving Educative!
 
         </div>
 
--- Frontend/src/screens/Home.jsx
+++ Frontend/src/screens/Home.jsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useEffect } from 'react';
 
 import Header from '../components/Header';
 import Post from '../components/Post';
+import styled from 'styled-components'
+
+function listCharacters() {
+    const [characters, setCharacters] = useState([])
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character")
+        .then(
+            response => response.json()
+        ).then(
+            data => {setCharacters(data.results); console.log(data.results)}
+        ).then(
+            error => console.log(error)
+        )
+    }, [])
+    return characters
+}
 
 
-export const Home = () => 
-    <div> 
-        <div>
-            <Header/>
-        </div>
+export const Home = () => {
+    const characters = listCharacters()
 
-        <div>
-            <Post/>
-        </div>
-    </div>
+    return (
+        <Div>
+            <h2>
+                <Header/>
+            </h2>
+            <Body>
+                {characters.map(item => (
+                    <div key={item.id}>
+                    <Post id={item.id} nickname= {item.name} image = {item.image}/>
+                    </div>
+                ))}
+            </Body>    
+            
+        </Div>
+        
+    );
+}
+
+const Body = styled.body`
+  background-color: #09f;
+ 
+`;
+const Div = styled.div`
+  background-color: #09f;
+ 
+`;
+    
 
