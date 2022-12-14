765b8e406f930d795de065e325dcbfeffca26586
Joaquin Martin <jmartinacu2002@gmail.com>
2022-12-14 08:22:56 +0100

    Variables entorno produccion y desarrollo configuradas. Y Empezado Dockerfile

--- Backend/M_Content/src/config.js
+++ Backend/M_Content/src/config.js
@@ -1,16 +1,19 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-import dotenv from 'dotenv'
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = await import('dotenv')
+  const { fileURLToPath } = await import('node:url')
+  const path = await import('node:path')
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename).slice(0, -4)
+  dotenv.config({ path: path.resolve(__dirname, '.env.' + process.env.NODE_ENV) })
+}
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config({ paht: path.resolve(__dirname, '/..', '/.env') })
-
+export const NODE_ENV = process.env.NODE_ENV || 'development'
 export const PORT = process.env.PORT || 3001
 export const DATABASE_HOST = process.env.DATABASE_HOST || 'localhost'
 export const DATABASE_PORT = process.env.DATABASE_PORT || 3306
-export const DATABASE_USERNAME = process.env.DATABASE_USERNAME || 'iissi_user'
-export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || 'iissi$user'
+export const DATABASE_USERNAME = process.env.DATABASE_USERNAME || 'user'
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || 'password'
 export const DATABASE_NAME = process.env.DATABASE_NAME || 'socialcontent'
+export const DATABASE_DIALECT = process.env.DATABASE_DIALECT || 'mariadb'
 export const BUCKET_NAME = process.env.BUCKET_NAME
 export const GOOGLE_KEYFILE = process.env.GOOGLE_KEYFILE
--- Backend/M_Content/src/helpers/sequelize.js
+++ Backend/M_Content/src/helpers/sequelize.js
@@ -1,16 +1,17 @@
 import { Sequelize } from 'sequelize'
 import {
-  DATABASE_HOST,
-  DATABASE_PORT,
+  DATABASE_NAME,
   DATABASE_USERNAME,
   DATABASE_PASSWORD,
-  DATABASE_NAME
+  DATABASE_PORT,
+  DATABASE_HOST,
+  DATABASE_DIALECT
 } from '../config.js'
 
 const database = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
   host: DATABASE_HOST,
   port: DATABASE_PORT,
-  dialect: 'mariadb',
+  dialect: DATABASE_DIALECT, // CUANDO SE CONECTE A GOOGLE CLOUD TIENE QUE CAMBIAR
   logging: false
 })
 
