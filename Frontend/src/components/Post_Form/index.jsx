9fff1a35602b433f47730eebf524ca6f18d350ba
carloosbaquero <carlos.baqueville@gmail.com>
2023-01-20 13:36:04 +0100

    PostForm does not work

--- Frontend/src/components/Post_Form/index.jsx
+++ Frontend/src/components/Post_Form/index.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Post from "../Post";
@@ -5,8 +6,18 @@ import Post from "../Post";
 
 export default function Post_Form() {
 
+    const [accessToken, setAccessToken] = useState(()=>{
+        const saved = localStorage.getItem("accessToken");
+        const initialValue = JSON.parse(saved);
+        return initialValue || "";
+    })
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
     const [description, setDescription] = useState('')
     const [image, setImage] = useState('')
+
+    const [succes, setSucces] = useState(false)
 
     const handleImage = (e) => {
         const file = e.currentTarget.files[0]
@@ -17,8 +28,21 @@ export default function Post_Form() {
             file})
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        try{
+            const res = await axios.post('http://localhost:3001/images', {name: image, description: description})
+            if (res.status === 204){
+                setSucces(true)
+            }
+        }catch(err){
+            console.log(err)
+            if (err.status === 404){
+                alert('Content not found')
+            } else if(err.status === 403) {
+                alert('User dont owe content')
+            }
+        }
         console.log(image);
         console.log(description)
     }
@@ -30,8 +54,14 @@ export default function Post_Form() {
         } 
 
     return (
-        <div className="SignIn">
-            <div className="auth-form-container">
+    <div className="SignIn">
+        {succes ? (
+            <div>
+                <h1>You succesfully posted</h1>
+            </div>
+        ) :(
+
+        <div className="auth-form-container">
             <div className="logo">
                 <h2 className="border">Free Space</h2>
                 <h2 className="wave">Free Space</h2>
@@ -44,7 +74,7 @@ export default function Post_Form() {
                 
                 {/* INPUT IMAGES */}
                 <label htmlFor="image">Select Image or Video:</label>
-                <input onChange={(e) => handleImage(e)} placeholder="image" id="image" name="image" type="file" accept='.jpg, .jpeg, .png' required></input>
+                <input onChange={(e) => handleImage(e)} placeholder="image" id="image" name="image" type="file" accept='.jpg, .png' required></input>
 
                 {/* SEND IMAGES TO BACKEND */}
                 <div>
@@ -52,7 +82,7 @@ export default function Post_Form() {
                 </div>
             </form>
             <button className="link-btn" onClick={handleClick}>Go Back </button>
-        </div>
+        </div> )}
       {/* VIEW POST */}
       {image == '' && <Post image={image} nickname='User' description={description}/>}
       {image != '' && <Post image={image.url} nickname='User' description={description}/>}
