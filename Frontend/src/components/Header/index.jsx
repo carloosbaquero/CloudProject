c4f515d6f8f4bd671b40d915bb57c078546a0622
carloosbaquero <carlos.baqueville@gmail.com>
2023-01-20 12:13:23 +0100

    Log In y Log Out Done

--- Frontend/src/components/Header/index.jsx
+++ Frontend/src/components/Header/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import {useNavigate} from 'react-router-dom'
-
+import axios from "axios";
+import { useLocalStorage } from "../Login";
 import "./Header.css";
 import styled from 'styled-components'
 
@@ -11,14 +12,45 @@ import styled from 'styled-components'
 
 const Header = () => {
 
+    const [accessToken, setAccessToken] = useState(()=>{
+        const saved = localStorage.getItem("accessToken");
+        const initialValue = JSON.parse(saved);
+        return initialValue || "";
+    })
+
+    // const [accessToken, setAccessToken] = useLocalStorage('accessToken', null)
+    // const [refreshToken, setRefreshToken] = useLocalStorage('refreshToken', null)
+
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
         const navigate = useNavigate();
  
         const handleClickUpload = () => {
             navigate('/upload');
         } 
 
-        const handleClickSignIn = () => {
-            navigate('/signin');
+
+
+        const handleClickSignIn = async () => {
+            if(accessToken!==null){
+                try{
+                    
+                    const res = await axios.delete("http://localhost:3002/users/logout")
+                    if (res.status === 204){
+                        console.log("Prueba")
+                        localStorage.removeItem('accessToken')
+                        localStorage.removeItem('refreshToken')
+                        console.log(accessToken)
+                        
+                    }
+                    
+                }catch(err){
+                    console.log(err)
+                    console.log(err.response.status)
+                } 
+            }      
+            navigate('/signin')
         } 
 
 
@@ -37,9 +69,15 @@ const Header = () => {
                 </nav>
 
                 <nav className="uploadbutton">
+                {accessToken ? (
+                <Button onClick={handleClickSignIn}>
+                    Log out
+                </Button>
+                ) : (
                 <Button onClick={handleClickSignIn}>
                     Sign In
                 </Button>
+                )}
                 </nav>
                 </div>
                 
