83b49ec5e2da18babd3669daf9f3d8e3b1065311
carloosbaquero <carlos.baqueville@gmail.com>
2023-01-23 15:29:47 +0100

    Production in localhost working

--- Frontend/FrontendProduction/server.js
+++ Frontend/FrontendProduction/server.js
@@ -2,7 +2,11 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html')
+})
 
 app.listen(80, () => {
   console.log('estoy escuchando por el purto 80')
