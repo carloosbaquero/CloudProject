e38fc80281939467c3a1cc7ff4629f2a67f341f2
Joaquin Martin <jmartinacu2002@gmail.com>
2023-01-24 11:31:28 +0100

    Imagenes dockerHub y entorno configurado frontend

--- Frontend/FrontendDevelopment/src/api/ContentHost.jsx
+++ Frontend/FrontendDevelopment/src/api/ContentHost.jsx
@@ -1,1 +1,4 @@
-export const M_CONTENT = 'http://localhost:3001'
+const M_CONTENT_HOST = process.env.M_CONTENT_HOST
+const M_CONTENT_PORT = process.env.M_CONTENT_PORT
+
+export const M_CONTENT = `${M_CONTENT_HOST}:${M_CONTENT_PORT}`
--- Frontend/FrontendDevelopment/src/api/UsersHost.jsx
+++ Frontend/FrontendDevelopment/src/api/UsersHost.jsx
@@ -1,1 +1,4 @@
-export const M_USERS = 'http://localhost:3002'
+const M_USERS_HOST = process.env.M_USERS_HOST
+const M_USERS_PORT = process.env.M_USERS_PORT
+
+export const M_USERS = `${M_USERS_HOST}:${M_USERS_PORT}`
