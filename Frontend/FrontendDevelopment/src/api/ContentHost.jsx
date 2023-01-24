71be35d61ffda2cc6de4b3709859ca16f1a3be60
Joaquin Martin <jmartinacu2002@gmail.com>
2023-01-24 15:37:25 +0100

    Frontend deployment working with evn

--- Frontend/FrontendDevelopment/src/api/ContentHost.jsx
+++ Frontend/FrontendDevelopment/src/api/ContentHost.jsx
@@ -1,5 +1,4 @@
-
-const M_CONTENT_HOST = process.env.M_CONTENT_HOST
-const M_CONTENT_PORT = process.env.M_CONTENT_PORT
+const M_CONTENT_HOST = import.meta.env.M_CONTENT_HOST
+const M_CONTENT_PORT = import.meta.env.M_CONTENT_PORT
 
 export const M_CONTENT = `${M_CONTENT_HOST}:${M_CONTENT_PORT}`
--- Frontend/FrontendDevelopment/src/api/UsersHost.jsx
+++ Frontend/FrontendDevelopment/src/api/UsersHost.jsx
@@ -1,4 +1,4 @@
-const M_USERS_HOST = process.env.M_USERS_HOST
-const M_USERS_PORT = process.env.M_USERS_PORT
+const M_USERS_HOST = import.meta.env.M_USERS_HOST
+const M_USERS_PORT = import.meta.env.M_USERS_PORT
 
 export const M_USERS = `${M_USERS_HOST}:${M_USERS_PORT}`
