d2f7775f5135f009e423feac1156a985616d6d34
carloosbaquero <carlos.baqueville@gmail.com>
2023-01-24 16:37:05 +0100

    Image frontend-free-space:v4

--- Frontend/FrontendDevelopment/src/api/UsersHost.jsx
+++ Frontend/FrontendDevelopment/src/api/UsersHost.jsx
@@ -1,4 +1,4 @@
 const M_USERS_HOST = import.meta.env.M_USERS_HOST
 const M_USERS_PORT = import.meta.env.M_USERS_PORT
 
-export const M_USERS = `${M_USERS_HOST}:${M_USERS_PORT}`
+export const M_USERS = `http://${M_USERS_HOST}:${M_USERS_PORT}`
